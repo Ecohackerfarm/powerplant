fbfbe03b6db71c835202b8367ef178ad9536e0e6
Simon Ever-Hale <severhal@oberlin.edu>
2017-07-10 18:04:21 +0200

    We have a server, we have a client, they are talking to each other

---
+++ server/app.js
@@ -0,0 +1,37 @@
+// import express
+import express from 'express';
+import path from 'path';
+// just updated to es2015 so now we can use import
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotWiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config.dev';
+
+var bodyParser = require('body-parser');
+
+// build our express app
+var app = express();
+
+var DIST_DIR = path.join(__dirname, "../dist");
+// set the static files location /public/img will be /img for users
+app.use(express.static(DIST_DIR));
+
+var compiler = webpack(webpackConfig);
+app.use(webpackMiddleware(compiler, {
+  hot: true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo: true
+}));
+app.use(webpackHotWiddleware(compiler));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// set up our routers
+app.use('/api', require('./routers/api'));
+// thank the LORD this works correctly
+app.get("*", function(req, res) {
+  res.sendFile(path.join(DIST_DIR, "index.html"));
+})
+
+module.exports = app;
---
+++ webpack.config.dev.js
@@ -0,0 +1,32 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'client'),
+        loaders: [ 'react-hot-loader', 'babel-loader' ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '*', '.js' ]
+  }
+}
