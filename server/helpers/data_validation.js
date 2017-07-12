34e877368687ee3d415dfac4bed0c5988322ea43
Simon Ever-Hale <severhal@oberlin.edu>
2017-07-12 16:12:43 +0200

    migrated data_validation

--- server/helpers/data_validation.js
+++ server/helpers/data_validation.js
@@ -2,19 +2,22 @@ import mongoose from 'mongoose';
 import Crop from '../models/crop';
 import Companionship from '../models/companionship';
 
-module.exports.idValidator = function(req, res, next) {
-  var ids = req.ids;
+import * as myself from './data_validation';
+export default myself;
+
+export function idValidator(req, res, next) {
+  const ids = req.ids;
   if (typeof ids === 'undefined') {
     next();
   }
   else {
-    var valid = ids.every(mongoose.Types.ObjectId.isValid);
+    const valid = ids.every(mongoose.Types.ObjectId.isValid);
 
     if (valid) {
       next();
     }
     else {
-      var err = new Error();
+      const err = new Error();
       err.status = 400;
       err.message = "Malformed object ID";
       next(err);
@@ -23,25 +26,21 @@ module.exports.idValidator = function(req, res, next) {
 }
 
 
-module.exports.getCompanionshipScores = function(snapshots, ids) {
+export function getCompanionshipScores(snapshots, ids) {
   // create an intersection of the companionship snapshots
   // crops with any negative interactions will have a value of 0
   // all other crops will give a percentage score which is how many they complement in the set
-  var result = {};
-  var maxScore = Companionship.schema.paths.compatibility.options.max;
-  console.log(maxScore);
-  var maxTotal = maxScore * snapshots.length;
-  for (var i=0; i<ids.length; i++) {
-    var data = snapshots[i];
-    var queryId = ids[i];
-    data.forEach(function(pair) {
+  const result = {};
+  const maxScore = Companionship.schema.paths.compatibility.options.max;
+  const maxTotal = maxScore * snapshots.length;
+  for (let i=0; i<ids.length; i++) {
+    const data = snapshots[i];
+    const queryId = ids[i];
+    data.forEach((pair) => {
       // look at the one that is NOT the corresponding id in ids
       // at the same index as the current snapshot
       // Because the current data is for the snapshot for a single crop
-      var id = pair.crop2;
-      if (id.equals(queryId)) {
-        id = pair.crop1;
-      }
+      const id = pair.crop2.equals(queryId) ? pair.crop1 : pair.crop2;
 
       // building the companionship scores, storing in result
       // if a companion crop is incompatible with any query crop, its score will be -1
@@ -62,29 +61,27 @@ module.exports.getCompanionshipScores = function(snapshots, ids) {
   return result;
 }
 
-module.exports.fetchModel = fetchModel;
-module.exports.fetchCrops = fetchModel(Crop, "crops");
-module.exports.fetchCropsWithCompanionships = fetchModel(Crop, "crops", "companionships");
-module.exports.fetchCompanionships = fetchModel(Companionship, "companionships", "crop1 crop2");
+export const fetchCrops = fetchModel(Crop, "crops");
+export const fetchCropsWithCompanionships = fetchModel(Crop, "crops", "companionships");
+export const fetchCompanionships = fetchModel(Companionship, "companionships", "crop1 crop2");
 
-module.exports.checkModel = checkModel;
-module.exports.checkCrops = checkModel(Crop);
-module.exports.checkCompanionships = checkModel(Companionship);
+export const checkCrops = checkModel(Crop);
+export const checkCompanionships = checkModel(Companionship);
 
 // assumes pre-validated ids!
-function fetchModel(model, resultName, populate) {
-  return function(req, res, next) {
+export function fetchModel(model, resultName, populate) {
+  return (req, res, next) => {
     if (typeof req.ids === 'undefined') {
       next();
       return;
     }
     req[resultName] = [];
-    req.ids.forEach(function(id) {
-      var query = model.findById(id);
+    req.ids.forEach((id) => {
+      let query = model.findById(id);
       if (typeof populate !== 'undefined') {
         query = query.populate(populate);
       }
-      query.exec(function(err, item) {
+      query.exec((err, item) => {
         if (item !== null) {
           req[resultName].push(item);
           if (req[resultName].length === req.ids.length) {
@@ -94,7 +91,7 @@ function fetchModel(model, resultName, populate) {
           }
         }
         else {
-          var error = new Error();
+          const error = new Error();
           error.status = 404;
           error.message = "No " + resultName + " with this ID found";
           next(error);
@@ -106,14 +103,14 @@ function fetchModel(model, resultName, populate) {
 }
 
 // function factory to check if ids exist in a given model
-function checkModel(model) {
-  return function(req, res, next) {
+export function checkModel(model) {
+  return (req, res, next) => {
     if (typeof req.ids === 'undefined') {
       next();
       return;
     }
-    var counter = 0;
-    req.ids.forEach(function(id) {
+    let counter = 0;
+    req.ids.forEach((id) => {
       model.count({_id: id}, function (err, count){
         if(count > 0){
           counter += 1;
@@ -130,8 +127,6 @@ function checkModel(model) {
   }
 }
 
-function escapeRegEx(text) {
+export function escapeRegEx(text) {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
-
-module.exports.escapeRegEx = escapeRegEx;
