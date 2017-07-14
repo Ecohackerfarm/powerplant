7b4cc69a1f66daacd7fd9ccc232d1da89ae74c10
Simon Ever-Hale <severhal@oberlin.edu>
2017-07-14 16:08:23 +0200

    sanity check for reducers

--- client/reducers/currLocation.js
+++ client/reducers/currLocation.js
@@ -1,5 +1,10 @@
 import {SET_CURRENT_LOCATION} from '/client/actions/types';
-
+import * as types from '/client/actions/types';
 export const currLocation = (state=null, action) => {
-  return action.location;
+  switch (action.type) {
+    case types.SET_CURRENT_LOCATION:
+      return action.location;
+    default:
+      return state;
+  }
 }
--- test/client/reducers/currLocation.js
+++ test/client/reducers/currLocation.js
@@ -1,10 +1,13 @@
 import {expect} from 'chai';
 import * as types from '/client/actions/types';
 import {currLocation} from '/client/reducers';
+import {expectNoActionForAllBut as sanityCheck} from './Helper';
 
 describe("currLocation reducer", () => {
+  const actions = [];
   describe("SET_CURRENT_LOCATION", () => {
     const type = types.SET_CURRENT_LOCATION;
+    actions.push(type);
     const location = "right here";
     it("should set the Location", () => {
       const action = {type, location};
@@ -12,6 +15,14 @@ describe("currLocation reducer", () => {
       const newState = currLocation(state, action);
       expect(newState).to.equal(location);
       expect(newState).not.to.equal(state);
+    });
+  });
+  describe("everything else", () => {
+    const location = "a location goes here";
+    it("should do nothing", () => {
+      const state = "before location";
+      const action = {location};
+      sanityCheck(currLocation, actions, state, action);
     });
   });
 });
