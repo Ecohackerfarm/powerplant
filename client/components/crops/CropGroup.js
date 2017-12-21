ccd3c953d20e5ce058610ff9918cf2533e8a8454
jsorribes82 <lo_sorry@hotmail.com>
2017-12-21 15:31:20 +0100

    change styles cropgroup

--- client/components/crops/CropGroup.js
+++ client/components/crops/CropGroup.js
@@ -2,23 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Typeahead } from 'react-bootstrap-typeahead';
 import style from 'react-bootstrap-typeahead/css/Typeahead.css';
-import { ToggleButton, ToggleButtonGroup, Row} from 'react-bootstrap';
+import { ToggleButton, ToggleButtonGroup, Row, Col} from 'react-bootstrap';
 
 class CropGroup extends React.Component {
   render(){
   	return(
-  		<Row>
-				<ToggleButtonGroup type="checkbox" defaultValue={[]}>
-					<ToggleButton value={true}>green when clicked</ToggleButton>
-				</ToggleButtonGroup>
-	  		<Typeahead
-					clearButton
-					multiple
-					options={this.props.cropGroup}
-					defaultSelected={this.props.cropGroup}
-					labelKey='commonName'
-					placeholder='Bed is empty, choose crop ...'
-				/>
+  		<Row className="button-checkbox-center">
+				<Col xs={3} mdOffset={2} md={2} >
+					<ToggleButtonGroup type="checkbox" defaultValue={[]}>
+						<ToggleButton value={true}></ToggleButton>
+					</ToggleButtonGroup>
+				</Col>
+				<Col xs={9} md={8}>
+					<Typeahead
+						clearButton
+						multiple
+						align='justify'
+						options={this.props.cropGroup}
+						defaultSelected={this.props.cropGroup}
+						labelKey='commonName'
+						placeholder='Bed is empty, choose crop ...'
+					/>
+				</Col>
 			</Row>
 	  );
   }
