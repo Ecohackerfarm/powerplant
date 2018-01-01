040405d1c5e1c1047f4a6d1673d0eb72c8ce62d4
jsorribes82 <lo_sorry@hotmail.com>
2018-01-02 00:04:32 +0100

    change styles page yours beds

--- client/components/beds/BedItem.js
+++ client/components/beds/BedItem.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Col, Panel } from 'react-bootstrap';
 
-export default function() {
-	return <p>Bed item</p>;
-}
+export default function({ item }) {
+	return (
+		<Col sm={12} md={6} lg={3}>
+			<Panel >
+				{item.plants.map( plant =>
+					<li>{plant.commonName}</li>)}
+			</Panel>
+		</Col>
+	);
+};
--- client/components/beds/BedsPage.js
+++ client/components/beds/BedsPage.js
@@ -9,18 +9,24 @@ import { connect } from 'react-redux';
 import CrudableList from '../shared/CrudableList';
 import AddBedForm from './AddBedForm';
 import BedItem from './BedItem';
+import { Grid } from 'react-bootstrap';
 import { createBed, deleteBed, editBed } from '/client/actions/bedActions';
-
-const BedsPage = function({ beds, match, location, actions }) {
+// const beds= {"0":{"name":"bedname", "plants":[{"commonName":"aaaaaasdfr"},{"commonName":"aaaaaasdfr22"},{"commonName":"aaaaaasdfr33"}]},"1":{"name":"bedname", "plants":[{"commonName":"aaaaaasdfr44"},{"commonName":"aaaaaasdfr55"},{"commonName":"aaaaaasdfr66"}]},"2":{"name":"bedname", "plants":[{"commonName":"aaaaaasdfr77"},{"commonName":"aaaaaasdfr88"},{"commonName":"aaaaaasdfr99"}]}}
+const BedsPage = function({  beds, match, location, actions }) {
 	return (
-		<CrudableList
-			actions={actions}
-			items={beds}
-			itemName="bed"
-			ItemListView={BedItem}
-			AddItemForm={AddBedForm}
-			match={match}
-		/>
+		<div className="yoursBeds">
+			<h3> Yours Beds</h3>
+			<Grid>
+				<CrudableList
+					actions={actions}
+					items={beds}
+					itemName="bed"
+					ItemListView={BedItem}
+					AddItemForm={AddBedForm}
+					match={match}
+				/>
+			</Grid>
+		</div>
 	);
 };
 
