773f32939e79248dec981621b13eac7e500b4c87
JustTB <franz@ecohackerfarm.org>
2018-01-11 00:49:13 +0100

    repairing git hooks script

--- .git_hooks/_init-hooks-path.sh
+++ .git_hooks/_init-hooks-path.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-gitPath="./.git"
-if ( -d "$gitPath") then
+#!/bin/bash
+GIT_PATH="./.git"
+if [ -d "$GIT_PATH" ]; then
   echo "Set git core.hooks to directory: .git_hooks"
   git config core.hooksPath .git_hooks
 fi
