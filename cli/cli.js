d7fc3dc02acb7e76dfcdb0cf31fb85785be56ce1
Petteri Pitkänen <petepitk@gmail.com>
2019-05-28 18:17:00 +0200

    Remove MongoDB-based code
    This is somewhat optimistic, but after initial tests with PouchDB I am
    quite convinced that PouchDB is a better fit for powerplant than MongoDB.

--- cli/cli.js
+++ cli/cli.js
@@ -3,41 +3,11 @@
  * @memberof cli
  */
 
-const mongoose = require('mongoose');
 const PouchDB = require('pouchdb');
-const {
-	setBaseUrl,
-	getCropsByName,
-	getUpdates,
-	getCrops,
-	getCropRelationships,
-	getCropTags,
-	getUsers,
-	addCrop,
-	addCropRelationship,
-	addCrops,
-	addCropRelationships,
-	addCropTags,
-	addUsers,
-	setCrops,
-	setCropRelationships,
-	setCropTags,
-	setUsers,
-	removeCrops,
-	removeCropRelationships,
-	removeCropTags,
-	removeUsers,
-	removeAllCrops,
-	removeAllCropRelationships
-} = require('../shared/api-client.js');
 const practicalplants = require('../db/practicalplants.js');
 const { plants, companions } = require('../db/companions.js');
 const { HTTP_SERVER_PORT, HTTP_SERVER_HOST } = require('../secrets.js');
-const { getDatabaseUrl, getHttpServerUrl, getPouchDatabaseUrl } = require('../shared/utils.js');
-const Crop = require('../server/models/crop.js');
-const CropRelationship = require('../server/models/crop-relationship.js');
-const CropTag = require('../server/models/crop-tag.js');
-const Version = require('../server/models/version.js');
+const { getHttpServerUrl, getPouchDatabaseUrl } = require('../shared/utils.js');
 
 /**
  * Print a message to console.
@@ -124,246 +94,6 @@ function getOptionArguments() {
 	return commandLineArguments.filter(
 		argument => !nonOptionArguments.includes(argument)
 	);
-}
-
-/**
- * Show documents.
- */
-async function doShow() {
-	const model = nonOptionArguments[1];
-	const ids = nonOptionArguments.slice(2);
-	const params = { ids: ids };
-
-	let responses;
-	switch (model) {
-		case 'crop': {
-			responses = await getCrops(params);
-			break;
-		}
-		case 'crop-relationship': {
-			responses = await getCropRelationships(params);
-			break;
-		}
-		case 'crop-tag': {
-			responses = await getCropTags(params);
-			break;
-		}
-		case 'user': {
-			responses = await getUsers(params);
-			break;
-		}
-		default:
-		break;
-	}
-
-	responses.forEach(response => {
-		console.log(response.data);
-	});
-}
-
-/**
- * Add documents.
- */
-async function doAdd() {
-	const model = nonOptionArguments[1];
-	const documents = parseOptionArray('document');
-	const params = { documents: documents };
-
-	let createdDocuments;
-	switch (model) {
-		case 'crop': {
-			createdDocuments = await addCrops(params);
-			break;
-		}
-		case 'crop-relationship': {
-			createdDocuments = await addCropRelationships(params);
-			break;
-		}
-		case 'crop-tag': {
-			createdDocuments = await addCropTags(params);
-			break;
-		}
-		case 'user': {
-			createdDocuments = await addUsers(params);
-			break;
-		}
-		default:
-		break;
-	}
-
-	console.log(createdDocuments.map(document => document.data));
-}
-
-/**
- * Update documents.
- */
-async function doUpdate() {
-	const model = nonOptionArguments[1];
-	const ids = nonOptionArguments.slice(2);
-	const documents = parseOptionArray('document');
-	const params = { documents: documents, ids: ids };
-
-	switch (model) {
-		case 'crop': {
-			await setCrops(params);
-			break;
-		}
-		case 'crop-relationship': {
-			await setCropRelationships(params);
-			break;
-		}
-		case 'crop-tag': {
-			await setCropTags(params);
-			break;
-		}
-		case 'user': {
-			await setUsers(params);
-			break;
-		}
-		default:
-		break;
-	}
-}
-
-/**
- * Remove documents.
- */
-async function doRemove() {
-	const model = nonOptionArguments[1];
-	const ids = nonOptionArguments.slice(2);
-	const params = { ids: ids };
-
-	switch (model) {
-		case 'crop': {
-			if (ids.length > 0) {
-				await removeCrops(params);
-			} else {
-				await removeAllCrops();
-			}
-			break;
-		}
-		case 'crop-relationship': {
-			if (ids.length > 0) {
-				await removeCropRelationships(params);
-			} else {
-				await removeAllCropRelationships();
-			}
-			break;
-		}
-		case 'crop-tag': {
-			await removeCropTags(params);
-			break;
-		}
-		case 'user': {
-			await removeUsers(params);
-			break;
-		}
-		default:
-		break;
-	}
-}
-
-/**
- * Get crops by name.
- */
-async function doGetCropsByName() {
-	const name = parseOption('name');
-	const index = parseOption('index');
-	const length = parseOption('length');
-
-	const crops = await getCropsByName({ name: name, index: index, length: length });
-
-	log(crops.data);
-}
-
-/**
- * Get updates from server.
- */
-async function doGetUpdates() {
-	const crops = parseInt(parseOption('crops'), 10);
-	
-	const version = {
-		crops: crops
-	};
-	
-	const updates = await getUpdates(version);
-	
-	log(updates);
-}
-
-/**
- * Migrate initial database through HTTP for stress testing the powerplant
- * server. The migration process may not be complete here, if you just want
- * to cleanly migrate the database, use the 'db-migrate' command.
- */
-async function migrate() {
-	const crops = practicalplants.readCrops();
-	
-	const plantNameToCrop = {};
-	crops.forEach(crop => {
-		crop.tags = [];
-		plantNameToCrop[crop.binomialName] = crop;
-	});
-
-	checkCompanionDatabaseIntegrity(plantNameToCrop);
-	
-	for (let index = 0; index < crops.length; index++) {
-		const response = await addCrop({ document: crops[index] });
-		Object.assign(crops[index], response.data);
-	}
-
-	const relationships = convertCompanionDatabaseToCropRelationships(plantNameToCrop);
-
-	for (let index = 0; index < relationships.length; index++) {
-		await addCropRelationship({ document: relationships[index] });
-	}
-}
-
-/**
- * Migrate the initial database directly to MongoDB.
- */
-async function dbMigrate() {
-	openMongooseConnection();
-
-	/*
-	 * Drop old collections and create new empty ones.
-	 */
-	await dropCollection('croprelationships');
-	await dropCollection('croptags');
-	await dropCollection('crops');
-	await dropCollection('versions');
-
-	await Crop.createCollection();
-	await CropRelationship.createCollection();
-	await CropTag.createCollection();
-	await Version.createCollection();
-
-	/*
-	 * Migrate crops.
-	 */
-	const crops = readCrops();
-	const cropDocuments = await Crop.insertMany(crops);
-
-	/*
-	 * Migrate crop relationships.
-	 */
-	const binomialNameToCropDocument = {};
-	cropDocuments.forEach(crop => {
-		binomialNameToCropDocument[crop.binomialName] = crop;
-	});
-
-	checkCompanionDatabaseIntegrity(binomialNameToCropDocument);
-
-	const relationships = convertCompanionDatabaseToCropRelationships(binomialNameToCropDocument);
-
-	const cropRelationshipDocuments = await CropRelationship.insertMany(relationships);
-
-	/*
-	 * Reset version information.
-	 */
-	await resetVersionCollection();
-
-	mongoose.connection.close();
 }
 
 async function pouchMigrate() {
@@ -510,31 +240,6 @@ function readCrops() {
 }
 
 /**
- * Reset version information. This forces the server to send updates to
- * clients.
- */
-async function dbResetVersion() {
-	openMongooseConnection();
-
-	await Version.createCollection();
-	await resetVersionCollection();
-	console.log('Reset version information');
-
-	mongoose.connection.close();
-}
-
-/**
- *
- */
-async function resetVersionCollection() {
-	await Version.deleteMany({}).exec();
-	const document = new Version();
-	document.crops = 0; // Force update to clients
-	document.cropRelationships = 0; // Force update to clients
-	await document.save();
-}
-
-/**
  * @param {Object} binomialNameToCropDocument
  * @return {Object[]}
  */
@@ -558,28 +263,6 @@ function checkCompanionDatabaseIntegrity(binomialNameToCrop) {
 	});
 }
 
-/**
- * @param name
- */
-async function dropCollection(name) {
-	try {
-		await mongoose.connection.dropCollection(name);
-	} catch (exception) {
-		// There'll be an exception if a collection doesn't exist.
-	}
-}
-
-/**
- *
- */
-function openMongooseConnection() {
-	const mongooseOptions = {
-		replicaSet: 'rs',
-		useNewUrlParser: true
-	};
-	mongoose.connect(getDatabaseUrl(), mongooseOptions);
-}
-
 /*
  * Global options
  */
@@ -594,15 +277,6 @@ let powerplantConfig = {
  * options for the command.
  */
 const commands = {
-	remove: doRemove,
-	add: doAdd,
-	update: doUpdate,
-	show: doShow,
-	'get-crops-by-name': doGetCropsByName,
-	'get-updates': doGetUpdates,
-	'migrate': migrate,
-	'db-migrate': dbMigrate,
-	'db-reset-version': dbResetVersion,
 	'pouch-migrate': pouchMigrate,
 	'pouch-sync': pouchSync,
 	'pouch-clone': pouchClone,
@@ -621,6 +295,5 @@ if (nonOptionArguments.length > 0) {
 	verbose = parseOption('verbose');
 	Object.assign(powerplantConfig, parseOption('powerplantConfig'));
 
-	setBaseUrl(powerplantConfig.host, powerplantConfig.port);
 	commands[nonOptionArguments[0]]();
 }
