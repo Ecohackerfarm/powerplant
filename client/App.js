4817bf723c719f93a34782bdabe569b46003c2cf
petteripitkanen <petteripitkanen@users.noreply.github.com>
2019-07-17 23:00:30 +0300

    Fix import/export to require (#58)
    * Use require instead of import/export for now
    
    * Add config.json to gitignore

--- client/App.js
+++ client/App.js
@@ -3,7 +3,7 @@
  * @memberof client
  */
 
-import Header from './views/Header';
+const Header = require('./views/Header');
 const React = require('react');
 const { connect } = require('react-redux');
 const { withRouter, Switch, Route, Redirect } = require('react-router-dom');
@@ -14,28 +14,37 @@ const AboutPage = require('./AboutPage.js');
  *
  */
 class App extends React.Component {
-	render() {
-		if (!this.props.storeLoaded) {
-			return (<div>Loading...</div>);
-		} else {
-			return (
-				<div>
-					<Header/>
-					<div>
-						<Switch>
-							<Route path="/" exact={true} render={() => <Redirect to="/about" />} />
-							<Route path="/crops" component={CropsPage} />
-							<Route path="/about" component={AboutPage} />
-						</Switch>
-					</div>
-				</div>
-			);
-		}
-	}
+  render() {
+    if (!this.props.storeLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <Header />
+          <div>
+            <Switch>
+              <Route
+                path="/"
+                exact={true}
+                render={() => <Redirect to="/about" />}
+              />
+              <Route path="/crops" component={CropsPage} />
+              <Route path="/about" component={AboutPage} />
+            </Switch>
+          </div>
+        </div>
+      );
+    }
+  }
 }
 
 const mapStateToProps = state => ({
-	storeLoaded: state.app.storeLoaded
+  storeLoaded: state.app.storeLoaded
 });
 
-module.exports = withRouter(connect(mapStateToProps, null)(App));
+module.exports = withRouter(
+  connect(
+    mapStateToProps,
+    null
+  )(App)
+);
--- client/views/Header.js
+++ client/views/Header.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import {LinkContainer} from 'react-router-bootstrap';
-import { Navbar, Nav } from 'react-bootstrap';
+const React = require('react');
+const { LinkContainer } = require('react-router-bootstrap');
+const { Navbar, Nav } = require('react-bootstrap');
 
-export default function Header (){
-    return (
-      <Navbar bg="light" collapseOnSelect="true" expand="lg">
-        <Navbar.Brand>powerplant</Navbar.Brand>
-        <Navbar.Collapse>
-          <Nav>
-            <LinkContainer to="/about"><Nav.Link>Home</Nav.Link></LinkContainer>
-            <LinkContainer to="/crops"><Nav.Link>Crops</Nav.Link></LinkContainer>
-            <LinkContainer to="/about"><Nav.Link>About</Nav.Link></LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    )
-}
+module.exports = function Header() {
+  return (
+    <Navbar bg="light" collapseOnSelect="true" expand="lg">
+      <Navbar.Brand>powerplant</Navbar.Brand>
+      <Navbar.Collapse>
+        <Nav>
+          <LinkContainer to="/about">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/crops">
+            <Nav.Link>Crops</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/about">
+            <Nav.Link>About</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
