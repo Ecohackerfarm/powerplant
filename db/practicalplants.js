11f46ec4d14b1f5444bda90f08bb72e573f5181e
Hitesh Walia <hiteshwar.walia@gmail.com>
2019-08-23 17:59:23 -0700

    fix issue 87

--- db/practicalplants.js
+++ db/practicalplants.js
@@ -105,6 +105,19 @@ function readCrops() {
       replaceValue(object, property, ['Yes', 'True'], 'true');
     });
 
+    for (const property of NAME_PROPERTIES) {
+      if (object[property]) {
+        if (/[a-z]/.test(object[property][0])) {
+          replaceValue(
+            object,
+            property,
+            [object[property]],
+            object[property][0].toUpperCase() + object[property].slice(1)
+          );
+        }
+      }
+    }
+
     replaceValue(object, 'matureMeasurementUnit', ['metres'], 'meters');
 
     replaceArrayValue(object['pollinators'], ['bees. self'], ['bees', 'self']);
